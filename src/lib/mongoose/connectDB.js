f3dd7a2ca49511493bc2608de1bc9ebd0c3b1be7
khudabakshk <khudabakshkhan625@gmail.com>
2025-01-24 22:07:03 +0500

    "D"

--- src/lib/mongoose/connectDB.js
+++ src/lib/mongoose/connectDB.js
@@ -12,7 +12,7 @@ export const connect = async () => {
 
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      dbName: 'BAKHS_DEBUG',
+      dbName: 'new-social',
     
     });
     console.log('Connected to MongoDB');
